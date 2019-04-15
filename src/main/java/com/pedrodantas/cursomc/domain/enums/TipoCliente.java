d71ee22a990cc119564fe19db25cc8743962982e
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-04-15 16:00:17 -0300

    Criando as classes, Cliente, Endereco e Enums

---
+++ src/main/java/com/pedrodantas/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,41 @@
+package com.pedrodantas.cursomc.domain.enums;
+
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod=cod;
+		this.descricao=descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public int getCod() {
+		return cod;
+	}
+	
+	public static TipoCliente ToEnum(Integer cod) {
+		if(cod==null) {
+			
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+
+}
