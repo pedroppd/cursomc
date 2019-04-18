ce92de79c5fd5d96c1a6e1d24a670fb458b43b8e
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-04-18 14:00:37 -0300

    Validação sintática com BeanValidation

--- src/main/java/com/pedrodantas/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedrodantas/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pedrodantas.cursomc.domain.Categoria;
+import com.pedrodantas.cursomc.dto.CategoriaDTO;
 import com.pedrodantas.cursomc.repositories.CategoriaRepository;
 import com.pedrodantas.cursomc.services.exception.DataIntegrityException;
 import com.pedrodantas.cursomc.services.exception.ObjectNotFoundException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);		
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 
