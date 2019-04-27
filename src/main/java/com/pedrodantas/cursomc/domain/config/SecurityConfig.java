3daba10ae75490fd9a1404b19d7c0e12382a7dec
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-04-27 16:01:43 -0300

    Implementando autenticação e geração do token jwt

--- src/main/java/com/pedrodantas/cursomc/domain/config/SecurityConfig.java
+++ src/main/java/com/pedrodantas/cursomc/domain/config/SecurityConfig.java
@@ -8,14 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.pedrodantas.cursomc.security.JWTAuthenticationFilter;
+import com.pedrodantas.cursomc.security.JwtUtil;
 
 
 @Configuration
@@ -24,10 +29,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	private static final String[] PUBLIC_MATCH = {
-			"/h2-console/**",
-			
+			"/h2-console/**"		
 	};
 	
 	
@@ -48,8 +58,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCH_GET).permitAll()
 		.antMatchers(PUBLIC_MATCH).permitAll().anyRequest().authenticated();
-		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 		
 	
